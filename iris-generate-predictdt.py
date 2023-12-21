b8ca0861604c74f825b42d56bfe9273ff61e323d
Han12496 <154412338+Han12496@users.noreply.github.com>
2023-12-21 14:26:03 +0800

    Update iris-generate-predictdt.py

--- iris-generate-predictdt.py
+++ iris-generate-predictdt.py
@@ -25,7 +25,7 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-loaded_model = pickle.load(open("Iris.h5", "rb")) #rb: read binary
+loaded_modeldt = pickle.load(open("Iris.h5", "rb")) #rb: read binary
 
 st.subheader('Class labels and their corresponding index number')
 st.write(Y.unique())
